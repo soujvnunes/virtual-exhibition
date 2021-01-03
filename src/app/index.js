a9c86676c4e0bdbf845b5027b39d1725fca02770
João Victor Nunes <soujvnunes@gmail.com>
2021-01-03 00:08:46 -0300

    Testing Typography.

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,34 @@
 import logo from "./logo.svg";
 import { root, header, logo as appLogo, link } from "./index.module.css";
+import Typography from "../typography";
 
 function App() {
   return (
     <div className={root}>
       <header className={header}>
         <img src={logo} className={appLogo} alt="logo" />
-        <p>
+        <Typography variant="title" weight="bolder">
+          This is a title.
+        </Typography>
+        <Typography variant="label" weight="bolder">
+          This is a label.
+        </Typography>
+        <Typography variant="caption">This is a caption.</Typography>
+        <Typography variant="caption" size="large">
+          This is a caption with large size.
+        </Typography>
+        <Typography size="small">
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          <a
-            className={link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </h1>
+        </Typography>
+        <Typography>
+          Edit <code>src/App.js</code> and save to reload.
+        </Typography>
+        <Typography size="large">
+          Edit <code>src/App.js</code> and save to reload.
+        </Typography>
+        <Typography className={link} href="https://reactjs.org">
+          Learn React
+        </Typography>
       </header>
     </div>
   );
