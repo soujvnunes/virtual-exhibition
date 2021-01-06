f55f0c941617a2a7c144f4e10077f3bb2e8ac72d
João Victor Nunes <soujvnunes@gmail.com>
2021-01-06 15:56:16 -0300

    Testing icons at Button.

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,6 @@
 import Button from "../button";
+import ButtonIcon from "../button-icon";
+import Icon from "../icon";
 import { useTheme } from "../modules";
 import Typography from "../typography";
 import { root } from "./index.module.css";
@@ -11,9 +13,23 @@ function App() {
       <Typography variant="title">This is a title.</Typography>
       <Typography>This is a paragraph.</Typography>
       <Typography href="https://reactjs.org">Learn React</Typography>
-      <Button onClick={() => setTheme(!theme)} variant="outlined">
+      <Button
+        onClick={() => setTheme(!theme)}
+        variant="outlined"
+        startIcon={<Icon name="face" />}
+      >
         I&apos;m a button
       </Button>
+      <Button
+        onClick={() => setTheme(!theme)}
+        variant="outlined"
+        endIcon={<Icon name="face" />}
+      >
+        I&apos;m a button
+      </Button>
+      <ButtonIcon onClick={() => setTheme(!theme)} variant="outlined">
+        <Icon name="face" />
+      </ButtonIcon>
     </div>
   );
 }
