44c184ea568a60b00f5fd2e9d901e0a2cad09a4e
João Victor Nunes <soujvnunes@gmail.com>
2021-01-06 14:28:58 -0300

    Adding outlined [variant] at Button.

--- src/button/index.js
+++ src/button/index.js
@@ -1,33 +1,42 @@
 import { forwardRef } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
-import { root } from "./index.module.css";
+import cssModules, { root } from "./index.module.css";
 import Typography from "../typography";
 import { toKebabCase } from "../modules";
 
-const Button = forwardRef(({ children, onClick, className, type }, ref) => (
-  <button
-    type="button"
-    className={clsx(root, className)}
-    id={toKebabCase({ value: children })}
-    name={toKebabCase({ value: children })}
-    {...{ ref, onClick, type }}
-  >
-    <Typography variant="label">{children}</Typography>
-  </button>
-));
+const Button = forwardRef(
+  ({ children, onClick, className, type, variant }, ref) => (
+    <button
+      type="button"
+      className={
+        (clsx(root, {
+          [cssModules[variant]]: variant,
+        }),
+        className)
+      }
+      id={toKebabCase({ value: children })}
+      name={toKebabCase({ value: children })}
+      {...{ ref, onClick, type }}
+    >
+      <Typography variant="label">{children}</Typography>
+    </button>
+  ),
+);
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
   className: PropTypes.string,
   type: PropTypes.oneOf(["button", "reset", "submit"]),
+  variant: PropTypes.oneOf(["outlined"]),
 };
 
 Button.defaultProps = {
-  onClick: () => {},
+  onClick: null,
   className: null,
   type: "button",
+  variant: null,
 };
 
 Button.displayName = "Button";
