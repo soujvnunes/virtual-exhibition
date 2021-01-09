e66e5f41d45c8f0497ccf1665844384ff73a5b37
João Victor Nunes <soujvnunes@gmail.com>
2021-01-09 13:14:15 -0300

    Moving theme to modules files as func.

--- src/modules.js
+++ src/modules.js
@@ -1,6 +1,31 @@
-// eslint-disable-next-line import/prefer-default-export
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { HEADING_TYPE } from "./constants";
+
 export function toKebabCase({ value = null } = {}) {
   return String(value)
     .toLowerCase()
     .replace(/[\s|']/g, (match) => ({ " ": "-", "'": "_" }[match]));
+}
+
+export function theme({ type } = {}) {
+  return responsiveFontSizes(
+    createMuiTheme({
+      palette: {
+        type,
+        primary: {
+          main: "#EA2934",
+        },
+        secondary: {
+          main: "#3F9CF9",
+        },
+      },
+      typography: {
+        fontFamily: [
+          "'Lucida Sans Unicode', -apple-system, BlinkMacSystemFont,	'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans'",
+        ].join(", "),
+        h2: { ...HEADING_TYPE, textTransform: "uppercase" },
+        button: HEADING_TYPE,
+      },
+    }),
+  );
 }
