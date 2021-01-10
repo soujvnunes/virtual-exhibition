a4f942bd61a29cf71a817f798e0e2d87dd540766
João Victor Nunes <soujvnunes@gmail.com>
2021-01-09 21:39:52 -0300

    Adding the progress Line.

---
+++ src/line/index.js
@@ -0,0 +1,18 @@
+import { LinearProgress } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+function Line(props) {
+  return <LinearProgress variant="determinate" {...props} />;
+}
+
+export default withStyles(({ palette, spacing }) => ({
+  root: {
+    height: spacing(0.25),
+  },
+  colorPrimary: {
+    backgroundColor: palette.action.disabled,
+  },
+  bar: {
+    backgroundColor: palette.text.primary,
+  },
+}))(Line);
