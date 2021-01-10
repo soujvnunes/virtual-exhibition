948787c04f535f8584cdaffbbc87df4757b5b845
João Victor Nunes <soujvnunes@gmail.com>
2021-01-09 22:48:06 -0300

    Testing the innerHeight reducer.

--- src/container/index.js
+++ src/container/index.js
@@ -1,17 +1,26 @@
+import { useEffect, useReducer } from "react";
 import { Container as MuiContainer, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "../header";
 import Content from "../content";
 import Footer from "../footer";
+import { reducer } from "../modules";
+import { REDUCER_HEIGHT } from "../constants";
 
 const useStyles = makeStyles(() => ({
   container: {
-    minHeight: "100vh",
+    minHeight: ({ height }) => height,
   },
 }));
 
 function Container() {
-  const { container } = useStyles();
+  const [{ height }, dispatch] = useReducer(reducer, { height: 0 });
+  const { container } = useStyles({ height });
+  const { innerHeight } = window;
+
+  useEffect(() => {
+    dispatch({ type: REDUCER_HEIGHT, payload: innerHeight });
+  }, [window]);
 
   return (
     <MuiContainer
