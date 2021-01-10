214a8c398d8b2fe4843a14e830ba177ee725cc3a
João Victor Nunes <soujvnunes@gmail.com>
2021-01-09 21:51:28 -0300

    Adjusting Content responsivity.

--- src/content/index.js
+++ src/content/index.js
@@ -1,12 +1,9 @@
-import { Container, Grid, Typography, useMediaQuery } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Logos from "../logos";
+import { _ } from "../modules";
 
 const useStyles = makeStyles(({ palette }) => ({
-  container: {
-    margin: "0 auto",
-    minHeight: "100vh",
-  },
   typographyTitle: {
     color: palette.text.secondary,
     letterSpacing: 6.853924,
@@ -17,31 +14,32 @@ const useStyles = makeStyles(({ palette }) => ({
   },
 }));
 
+const gridOffset = _("xs down") && <Grid item xs={2} />;
+
 function Content() {
-  const { container, typographyTitle, logos } = useStyles();
-  const mdDown = useMediaQuery(({ breakpoints }) => breakpoints.down("md"));
+  const classes = useStyles();
+  const alignCenter = _("sm down") && { align: "center" };
+  const heading6 = _("sm down") && "h6";
+  const heading4 = _("md down") && "h4";
 
   return (
-    <Grid
-      className={container}
-      component={Container}
-      container
-      alignItems="center"
-      spacing={mdDown ? 0 : 4}
-    >
-      <Grid item md={7} xs={12}>
-        <Logos className={logos} />
+    <Grid container alignItems="center">
+      {gridOffset}
+      <Grid item sm={7} xs={8}>
+        <Logos className={classes.logos} />
       </Grid>
-      <Grid item md={5} xs={12}>
+      {gridOffset}
+      <Grid item sm={5} xs={12}>
         <Typography
-          className={typographyTitle}
-          variant="h2"
+          className={classes.typographyTitle}
+          component="h2"
+          variant={heading6 || heading4 || "h2"}
           gutterBottom
-          {...(mdDown && { align: "center" })}
+          {...alignCenter}
         >
           Veja o quanto evoluímos juntos.
         </Typography>
-        <Typography {...(mdDown && { align: "center" })}>
+        <Typography variant={_("sm down") ? "body2" : "body1"} {...alignCenter}>
           Já passaram 60 anos! Explore através do decorrer das décadas o valor e
           prestígio da nossa Universidade Federal de Alagoas expandindo-se.
         </Typography>
