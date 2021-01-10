f6421b40ce61a1854ec09f9bafc95ecdac50826d
João Victor Nunes <soujvnunes@gmail.com>
2021-01-10 15:24:46 -0300

    Using the window size hook to set the y Container.

--- src/container/index.js
+++ src/container/index.js
@@ -1,39 +1,35 @@
-import { useEffect, useReducer } from "react";
-import { Container as MuiContainer, Grid } from "@material-ui/core";
+import { Box, Container as MuiContainer, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "../header";
 import Content from "../content";
 import Footer from "../footer";
-import { reducer } from "../modules";
-import { REDUCER_HEIGHT } from "../constants";
+import { useWindowSize } from "../modules";
 
 const useStyles = makeStyles(() => ({
   container: {
-    minHeight: ({ height }) => height,
+    minHeight: ({ minHeight }) => minHeight,
   },
 }));
 
 function Container() {
-  const [{ height }, dispatch] = useReducer(reducer, { height: 0 });
-  const { container } = useStyles({ height });
-  const { innerHeight } = window;
-
-  useEffect(() => {
-    dispatch({ type: REDUCER_HEIGHT, payload: innerHeight });
-  }, [window]);
+  const classes = useStyles({
+    minHeight: useWindowSize("height"),
+  });
 
   return (
-    <MuiContainer
-      className={container}
-      component={Grid}
-      container
-      direction="column"
-      justify="space-between"
-    >
-      <Header />
-      <Content />
-      <Footer />
-    </MuiContainer>
+    <Box position="fixed" top={0} left={0} bottom={0} right={0} zIndex="drawer">
+      <MuiContainer
+        className={classes.container}
+        component={Grid}
+        container
+        direction="column"
+        justify="space-between"
+      >
+        <Header />
+        <Content />
+        <Footer />
+      </MuiContainer>
+    </Box>
   );
 }
 
