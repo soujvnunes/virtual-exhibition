218c6e90a79a6ca1a0e658b7b5439026d559ead4
João Victor Nunes <soujvnunes@gmail.com>
2021-01-12 16:21:30 -0300

    Using Line without wrapping it.

--- src/line/index.js
+++ src/line/index.js
@@ -8,6 +8,7 @@ function Line(props) {
 export default withStyles(({ palette, spacing }) => ({
   root: {
     height: spacing(0.25),
+    width: "100%",
   },
   colorPrimary: {
     backgroundColor: palette.action.disabled,
