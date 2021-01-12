b40ec690847fd9a2e63bfbb6328a2cbaa1eb721e
João Victor Nunes <soujvnunes@gmail.com>
2021-01-12 16:20:57 -0300

    Wrapping mainly component at Main.

---
+++ src/main/index.js
@@ -0,0 +1,13 @@
+import AppBar from "../app-bar";
+import HeroScroll from "../hero-scroll";
+
+function Main() {
+  return (
+    <main>
+      <AppBar />
+      <HeroScroll />
+    </main>
+  );
+}
+
+export default Main;
