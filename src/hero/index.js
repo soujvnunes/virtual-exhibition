4b6a5a06cc1d1dd9c5f200d8cb52386ba68e113d
João Victor Nunes <soujvnunes@gmail.com>
2021-01-13 14:07:11 -0300

    Refactoring Hero after Intro creation.

--- src/hero/index.js
+++ src/hero/index.js
@@ -1,122 +1,30 @@
-import { Container, Grid, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
-import { Fragment } from "react";
-import Logos from "../logos";
-import { useWindowDimension, _ } from "../modules";
-import { Rapport } from "../asset";
 import Gallery from "../gallery";
+import Section from "../section";
+import SectionTitle from "../section-title";
+import SectionParagraph from "../section-paragraph";
 
-const useStyles = makeStyles(({ palette, transitions }) => ({
-  root: ({ image }) =>
-    image && {
-      backgroundImage: `url(${image})`,
-      position: "relative",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      transition: transitions.create(["background-position-y"], {
-        duration: transitions.duration.short,
-      }),
-      "&:before": {
-        backgroundColor: "rgba(0, 0, 0, 0.8)",
-        content: "''",
-        position: "absolute",
-        inset: 0,
-      },
-    },
-  container: {
-    position: "relative",
-  },
-  grid: {
-    minHeight: ({ minHeight }) => minHeight,
-  },
-  typographyTitle: {
-    color: palette.text.secondary,
-    letterSpacing: 6.853924,
-    "& > span": {
-      WebkitBackgroundClip: "text",
-      textFillColor: "transparent",
-      backgroundImage: `url(${Rapport})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    },
-  },
-  logos: {
-    width: "inherit",
-    height: "inherit",
-  },
-}));
-
-function Hero({ title, description, gallery, intro }) {
-  const classes = useStyles({
-    minHeight: useWindowDimension("height"),
-    image: gallery && gallery[0].img,
-  });
-  const alignCenter = intro
-    ? _("sm down") && { align: "center" }
-    : { align: "center" };
-  const heading6 = _("sm down") && "h6";
-  const heading4 = _("md down") && "h4";
-  const gridOffsetIntro = _("xs down") && <Grid item xs={2} />;
-  const gridOffsetContent = !intro && <Grid item xs={3} />;
-  const renderTitle = title.split("/").map((sentence, index) => {
-    const commonProps = {
-      key: sentence,
-      children: sentence,
-    };
-
-    if (index === 1) {
-      return <span {...commonProps} />;
-    }
-
-    return <Fragment {...commonProps} />;
-  });
-  const renderIntro = intro && (
-    <>
-      {gridOffsetIntro}
-      <Grid item sm={7} xs={8}>
-        <Logos className={classes.logos} />
-      </Grid>
-      {gridOffsetIntro}
-    </>
-  );
-  const renderContent = (
-    <>
-      {gridOffsetContent}
-      <Grid item sm={intro ? 5 : 6} xs={12}>
-        <Typography
-          className={classes.typographyTitle}
-          component="h2"
-          variant={heading6 || heading4 || "h2"}
-          gutterBottom
-          {...alignCenter}
-        >
-          {renderTitle}
-        </Typography>
-        <Typography variant={_("sm down") ? "body2" : "body1"} {...alignCenter}>
-          {description}
-        </Typography>
-      </Grid>
-      {gridOffsetContent}
-    </>
-  );
+function Hero({ title, description, gallery }) {
+  const gridOffset = <Grid item xs={3} />;
 
   return (
-    <section className={classes.root}>
-      <Container className={classes.container}>
-        <Grid
-          className={classes.grid}
-          container
-          alignContent="center"
-          alignItems="center"
-          justify="center"
-        >
-          {renderIntro}
-          {renderContent}
-          {!intro && <Gallery photos={gallery} />}
+    <Section>
+      <Grid
+        container
+        alignContent="center"
+        alignItems="center"
+        justify="center"
+      >
+        {gridOffset}
+        <Grid item sm={6} xs={12}>
+          <SectionTitle>{title}</SectionTitle>
+          <SectionParagraph>{description}</SectionParagraph>
         </Grid>
-      </Container>
-    </section>
+        {gridOffset}
+        <Gallery photos={gallery} />
+      </Grid>
+    </Section>
   );
 }
 
@@ -124,7 +32,6 @@ Hero.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   gallery: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string])),
-  intro: PropTypes.bool.isRequired,
 };
 
 Hero.defaultProps = {
