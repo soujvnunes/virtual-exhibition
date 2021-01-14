93d62d46f9a49261870be6521c4975a921e937fb
João Victor Nunes <soujvnunes@gmail.com>
2021-01-13 22:05:34 -0300

    Settinf of only dark theme.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
-import { useMemo } from "react";
-import { CssBaseline, useMediaQuery } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./modules";
 import Main from "./main";
-import { KEY_THEME_DARK, KEY_THEME_LIGHT } from "./constants";
+import { KEY_THEME_DARK } from "./constants";
 import Navigation from "./navigation";
 
 function App() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const type = prefersDarkMode ? KEY_THEME_DARK : KEY_THEME_LIGHT;
-  const memoizedTheme = useMemo(() => theme({ type }), [prefersDarkMode]);
-
   return (
-    <ThemeProvider theme={memoizedTheme}>
+    <ThemeProvider theme={theme({ type: KEY_THEME_DARK })}>
       <CssBaseline />
       <Navigation />
       <Main />
