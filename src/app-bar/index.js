8372ac8d1c016d37a19e9679683492e8f9434e91
João Victor Nunes <soujvnunes@gmail.com>
2021-01-14 11:38:18 -0300

    Setting of Logos visibility.

--- src/app-bar/index.js
+++ src/app-bar/index.js
@@ -9,26 +9,28 @@ import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "../icon-button";
 import Button from "../button";
 import Logos from "../logos";
+import { useWindowDimension } from "../modules";
 
 const useStyles = makeStyles(({ spacing }) => ({
-  iconButtonLogos: {
-    marginRight: "auto",
-  },
   iconButtonAccessibility: {
+    marginLeft: "auto",
     marginRight: spacing(1),
   },
 }));
 
 function AppBar() {
-  const classes = useStyles();
+  const { iconButtonAccessibility } = useStyles();
+  const showLogos = useWindowDimension("scroll") >= 900;
 
   return (
     <MuiAppBar color="transparent" elevation={0}>
       <Toolbar component={Container}>
-        <MuiIconButton className={classes.iconButtonLogos} edge="start">
-          <Logos variant="outlined" />
-        </MuiIconButton>
-        <IconButton className={classes.iconButtonAccessibility}>
+        {showLogos && (
+          <MuiIconButton edge="start">
+            <Logos variant="outlined" />
+          </MuiIconButton>
+        )}
+        <IconButton classes={{ root: iconButtonAccessibility }}>
           <Icon>accessibility</Icon>
         </IconButton>
         <Button variant="outlined" endIcon={<Icon>chevron_right</Icon>}>
