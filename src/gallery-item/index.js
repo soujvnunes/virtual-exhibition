c5c3e7f6b859cf70e17c747defcca5f3e63d8b96
João Victor Nunes <soujvnunes@gmail.com>
2021-01-14 11:35:32 -0300

    Adding blur resource to backdrop and item on hover.

--- src/gallery-item/index.js
+++ src/gallery-item/index.js
@@ -4,23 +4,29 @@ import {
   Dialog,
   DialogContent,
   DialogContentText,
+  Grid,
+  Icon,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
+const backdropFilter = "saturate(160%) brightness(32%) blur(8px)";
+
 const useStyles = makeStyles(({ spacing, palette, transitions }) => ({
   root: {
+    backdropFilter,
     overflow: "hidden",
     borderRadius: spacing(3),
     borderStyle: "solid",
     borderWidth: spacing(0.25),
-    borderColor: ({ hover }) => (hover ? palette.text.primary : "transparent"),
+    borderColor: ({ hover }) =>
+      hover ? palette.action.active : palette.action.disabled,
     transition: transitions.create(["border-color"]),
-    "&:hover img": {
-      opacity: ({ hover }) => (hover ? 0 : 1),
-      transition: transitions.create(["opacity"]),
-    },
+  },
+  cardMedia: {
+    opacity: ({ hover }) => (hover ? 0 : 1),
+    transition: transitions.create(["opacity"]),
   },
   paper: {
     backgroundColor: "transparent",
@@ -37,13 +43,36 @@ const useStyles = makeStyles(({ spacing, palette, transitions }) => ({
     position: "absolute",
     width: "100%",
     bottom: 0,
+    "& p": {
+      margin: 0,
+    },
+  },
+  backdrop: {
+    backdropFilter,
+    backgroundColor: "transparent",
+  },
+  fullScreen: {
+    position: "absolute",
+    inset: 0,
+    opacity: ({ hover }) => (hover ? 1 : 0),
+    transition: transitions.create(["opacity"]),
   },
 }));
 
 function GalleryItem({ image, alt }) {
   const [open, setOpen] = useState(false);
   const [hover, setHover] = useState(false);
-  const { root, paper, dialogContent } = useStyles({ hover, image });
+  const {
+    root,
+    paper,
+    dialogContent,
+    backdrop,
+    fullScreen,
+    cardMedia,
+  } = useStyles({
+    hover,
+    image,
+  });
   const handleClick = () => setOpen((prevState) => !prevState);
   const handleMouseEnter = () => setHover(true);
   const handleMouseLeave = () => setHover(false);
@@ -56,13 +85,27 @@ function GalleryItem({ image, alt }) {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <CardMedia component="img" height="112" {...{ image, alt }} />
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          className={fullScreen}
+        >
+          <Icon>fullscreen</Icon>
+        </Grid>
+        <CardMedia
+          classes={{ root: cardMedia }}
+          component="img"
+          height="112"
+          {...{ image, alt }}
+        />
       </CardActionArea>
       <Dialog
         maxWidth="lg"
         onClose={handleClick}
         open={open}
         classes={{ paper }}
+        BackdropProps={{ classes: { root: backdrop } }}
       >
         <div style={{ width: "100vw", height: "100vh" }} />
         <DialogContent classes={{ root: dialogContent }}>
