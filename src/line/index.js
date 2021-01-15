98f218b2c98a2fd51b37bd0a977663363dc197db
João Victor Nunes <soujvnunes@gmail.com>
2021-01-15 00:29:59 -0300

    Line: setting of proptypes.

--- src/line/index.js
+++ src/line/index.js
@@ -1,9 +1,14 @@
 import { LinearProgress } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
-function Line(props) {
-  return <LinearProgress variant="determinate" {...props} />;
+function Line({ value, ...props }) {
+  return <LinearProgress variant="determinate" {...{ value, ...props }} />;
 }
+
+Line.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
 export default withStyles(({ palette, spacing }) => ({
   root: {
