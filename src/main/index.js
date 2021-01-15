c090c883c9b45d18ee713c1658bdb11d784b6bc9
João Victor Nunes <soujvnunes@gmail.com>
2021-01-15 00:30:15 -0300

    Moving plain components to main.

---
+++ src/main/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppBar from "../app-bar";
+import Intro from "../intro";
+import Hero from "../hero";
+
+function Main() {
+  return (
+    <main>
+      <AppBar />
+      <Intro />
+      <Hero />
+    </main>
+  );
+}
+
+export default Main;
