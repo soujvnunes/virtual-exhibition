1862290180ffff04ea9a1b2926562e08644d63c6
João Victor Nunes <soujvnunes@gmail.com>
2021-01-15 14:46:03 -0300

    Using color and variants according of NavDecade progress/selection.

--- src/nav-decade/index.js
+++ src/nav-decade/index.js
@@ -1,19 +1,29 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { Button, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { findIndex } from "lodash";
+import { useContext } from "react";
 import NavLabel from "../nav-label";
 import NavRule from "../nav-rule";
 import { getHeroes } from "../modules";
+import { HERO_CONTEXT } from "../constants";
 
 const useStyles = makeStyles(() => ({
   root: {
     minWidth: "auto",
+    padding: 0,
   },
 }));
 
 function NavDecade({ shortTitle, onClick, index, ...props }) {
+  const { hero } = useContext(HERO_CONTEXT);
   const { root } = useStyles();
+  const variantLarge = {
+    ...(findIndex(getHeroes(), hero) === index && { variant: "large" }),
+  };
+  const colorPrimary = {
+    ...(findIndex(getHeroes(), hero) >= index && { color: "primary" }),
+  };
 
   return (
     <Grid
@@ -25,9 +35,15 @@ function NavDecade({ shortTitle, onClick, index, ...props }) {
       id={shortTitle}
       {...{ onClick, ...props }}
     >
-      <NavLabel htmlFor={shortTitle}>{shortTitle}</NavLabel>
-      <NavRule />
-      <NavRule variant="small" unrendered={getHeroes().length === index + 1} />
+      <NavLabel {...variantLarge} htmlFor={shortTitle}>
+        {shortTitle}
+      </NavLabel>
+      <NavRule {...colorPrimary} {...variantLarge} />
+      <NavRule
+        {...colorPrimary}
+        variant="small"
+        unrendered={getHeroes().length === 0}
+      />
     </Grid>
   );
 }
