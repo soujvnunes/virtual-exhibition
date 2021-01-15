be7c06d57b2c86e491ee8cfb0c5cb8840d5218a7
João Victor Nunes <soujvnunes@gmail.com>
2021-01-14 22:00:56 -0300

    HeroScroll: cleaning-up development.

--- src/hero-scroll/index.js
+++ src/hero-scroll/index.js
@@ -2,15 +2,9 @@ import PropTypes from "prop-types";
 import Hero from "../hero";
 
 function HeroScroll({ heroes }) {
-  console.log(heroes);
-
-  return (
-    <>
-      {heroes.slice(0, 1).map(({ id, ...hero }) => (
-        <Hero key={id} {...hero} />
-      ))}
-    </>
-  );
+  return heroes
+    .slice(0, 1)
+    .map(({ id, ...hero }) => <Hero key={id} {...hero} />);
 }
 
 HeroScroll.propTypes = {
