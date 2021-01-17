eaba6e49418ea4c297795a5ac3b02e8bc496afb3
João Victor Nunes <soujvnunes@gmail.com>
2021-01-16 21:28:07 -0300

    NavLabel: adding transitions to changer props.

--- src/nav-label/style.js
+++ src/nav-label/style.js
@@ -1,9 +1,10 @@
 import { makeStyles } from "@material-ui/core";
 
-export default makeStyles(({ spacing }) => ({
+export default makeStyles(({ spacing, transitions }) => ({
   root: {
     position: "absolute",
     marginTop: ({ size }) => spacing(size === "large" ? -4 : -3),
     top: 0,
+    transition: transitions.create(["margin-top", "font-size"]),
   },
 }));
