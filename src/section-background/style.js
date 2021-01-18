1d70948ce84a8d8cae998b04c69245df6a1bcb63
João Victor Nunes <soujvnunes@gmail.com>
2021-01-17 21:17:26 -0300

    SectionBackground: refactoring backdrop technique.

--- src/section-background/style.js
+++ src/section-background/style.js
@@ -2,15 +2,10 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
   root: {
-    backgroundImage: ({ background }) => `url(${background})`,
     position: "relative",
+    backgroundImage: ({ background }) =>
+      `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${background})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    "&:before": {
-      backgroundColor: "rgba(0, 0, 0, 0.8)",
-      content: "''",
-      position: "absolute",
-      inset: 0,
-    },
   },
 }));
