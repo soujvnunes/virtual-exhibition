f5bf7804fd80c3fe9ef5d01ad0cde8e89235fcff
João Victor <soujvnunes@gmail.com>
2021-01-20 18:21:54 -0300

    Section: moving styles to useStyles.

--- src/section/index.js
+++ src/section/index.js
@@ -1,21 +1,17 @@
 import { Container } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import useStyles from "./style";
 
 function Section({ id, ...props }) {
-  return <Container component="section" {...{ ...props, id }} />;
+  const { root } = useStyles();
+
+  return (
+    <Container classes={{ root }} component="section" {...{ ...props, id }} />
+  );
 }
 
 Section.propTypes = {
   id: PropTypes.string.isRequired,
 };
 
-export default withStyles(({ spacing, mixins }) => ({
-  root: {
-    minHeight: "100vh",
-    display: "flex",
-    position: "relative",
-    paddingTop: spacing(mixins.toolbar.minHeight / 8),
-    paddingBottom: spacing(mixins.toolbar.minHeight / 8),
-  },
-}))(Section);
+export default Section;
