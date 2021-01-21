8c077cb4177a0f30790a78d3b64369e51040eb27
João Victor <soujvnunes@gmail.com>
2021-01-21 20:49:54 -0300

    Adding and adjusting description at Hero.

---
+++ src/hero/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(({ spacing }) => ({
+  section: {
+    paddingBottom: spacing(16),
+  },
+  h6: {
+    marginTop: spacing(1.5),
+  },
+  progress: {
+    marginTop: spacing(1.5),
+  },
+}));
