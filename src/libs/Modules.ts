5c8129574392701ca003b89a0510dc8e706649fb
soujvnunes <soujvnunes@gmail.com>
2021-07-13 11:38:41 -0300

    Enhancing collections getter on lib

--- src/libs/Modules.ts
+++ src/libs/Modules.ts
@@ -1,4 +1,34 @@
+import { Collections } from "libs";
+
 export default {
+  getExhibition() {
+    return Collections.exhibition.title.map((title, exhibitionIndex) => ({
+      title,
+      dean: Collections.exhibition.dean.alt[exhibitionIndex].map(
+        (alt, deanIndex) => ({
+          alt,
+          src: Collections.exhibition.dean.src[exhibitionIndex][deanIndex],
+          year: Collections.exhibition.dean.year[exhibitionIndex][deanIndex],
+          description:
+            Collections.exhibition.dean.description[exhibitionIndex][deanIndex],
+        }),
+      ),
+      gallery: Collections.exhibition.gallery.alt[exhibitionIndex].map(
+        (alt, galleryIndex) => ({
+          alt,
+          src: Collections.exhibition.gallery.src[exhibitionIndex][
+            galleryIndex
+          ],
+        }),
+      ),
+    }));
+  },
+  getTributes() {
+    return Collections.tributes.title.map((_, index) => ({
+      title: Collections.tributes.title[index],
+      src: Collections.tributes.src[index],
+    }));
+  },
   useCache({ key = "" }: { key: string }) {
     const cachedValue: string | null = localStorage.getItem(key);
 
@@ -21,9 +51,6 @@ export default {
       }
     }
 
-    return [
-      (cachedValue && JSON.parse(cachedValue)) || null,
-      setCache,
-    ] as const;
+    return [cachedValue && JSON.parse(cachedValue), setCache] as const;
   },
 };
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,3 +1,4 @@
 export { default as Constants } from "./Constants";
+export { default as Collections } from "./Collections";
 export { default as Modules } from "./Modules";
 export { default as Texts } from "./Texts";
