19c34301059c420436489a7c324a91d1396bdd60
soujvnunes <soujvnunes@gmail.com>
2021-08-22 13:45:26 -0300

    Adding initial home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,40 @@
+import { Button, Typography } from "components";
+import { useMode } from "hooks";
+
+function Home() {
+  const mode = useMode();
+
+  return (
+    <>
+      <Button onClick={() => mode.dispatch({ type: useMode.types.toggle })}>
+        {mode.is}
+      </Button>
+      <Button
+        onClick={() =>
+          mode.dispatch({ type: useMode.types.set, props: "dark" })
+        }>
+        {mode.is}
+      </Button>
+      <Typography as="h1" gutterBottom>
+        Heading 3
+      </Typography>
+      <Typography as="h2" gutterBottom>
+        Heading 2
+      </Typography>
+      <Typography as="h3" gutterBottom>
+        Heading 1
+      </Typography>
+      <Typography variant="p" gutterBottom>
+        Paragraph
+      </Typography>
+      <Typography weight="bold" size="large" gutterBottom>
+        Span
+      </Typography>
+      <Typography variant="h2" weight="bold">
+        Span
+      </Typography>
+    </>
+  );
+}
+
+export default Home;
