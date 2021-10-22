2a6bd39255c44de54f0ffdfcbe243d37e4662f1f
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-22 20:39:41 -0300

    Set Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Button, Layout } from "ui";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+function Home() {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <Button>test</Button>
+    </Layout>
+  );
+}
 
-export default IndexPage
+export default Home;
