0858d33850bcc5b2bb8b59ff3cfbc888175003d4
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-22 20:38:06 -0300

    Adding assets

---
+++ utils/get-tributes.ts
@@ -0,0 +1,23 @@
+function getTributes(
+  tributes = {
+    src: [
+      "https://www.youtube.com/embed/jr-EcARAio4",
+      "https://www.youtube.com/embed/RYb0tvEygNo",
+      "https://www.youtube.com/embed/u22TU6KEgHc",
+      "https://www.youtube.com/embed/V7N8akgwPJA",
+    ],
+    title: [
+      "Ufal 60 Anos: raízes conectad@s com o futuro",
+      "Ufal 60 Anos-Raízes",
+      "Servidoras parabenizam Ufal pelos 60 anos",
+      "Professores parabenizam Ufal pelos seus 60 anos",
+    ],
+  },
+) {
+  return tributes.title.map((title: string, index: number) => ({
+    title,
+    src: tributes.src[index],
+  }));
+}
+
+export default getTributes;
---
+++ utils/index.ts
@@ -0,0 +1,2 @@
+export { default as getExhibition } from "./get-exhibition";
+export { default as getTributes } from "./get-tributes";
