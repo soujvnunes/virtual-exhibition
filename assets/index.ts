58ef607b50cc113a288cd924d195ad181d29899d
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-23 20:50:46 -0300

    Enhance assets imports

--- assets/index.ts
+++ assets/index.ts
@@ -1,71 +1,148 @@
-export { default as de1960s01 } from "./decada-0060-0001-min.jpg";
-export { default as de1960s02 } from "./decada-0060-0002-min.jpg";
-export { default as de1960s03 } from "./decada-0060-0003-min.jpg";
-export { default as de1960s04 } from "./decada-0060-0004-min.jpg";
-export { default as de1960s05 } from "./decada-0060-0005-min.jpg";
-export { default as de1960s06 } from "./decada-0060-0006-min.jpg";
-export { default as de1960s07 } from "./decada-0060-0007-min.jpg";
-export { default as de1960s08 } from "./decada-0060-0008-min.jpg";
-export { default as de1960s09 } from "./decada-0060-0009-min.jpg";
-export { default as de1960s10 } from "./decada-0060-0010-min.jpg";
-export { default as de1970s01 } from "./decada-0070-0001-min.jpg";
-export { default as de1970s02 } from "./decada-0070-0002-min.jpg";
-export { default as de1970s03 } from "./decada-0070-0003-min.jpg";
-export { default as de1970s04 } from "./decada-0070-0004-min.jpg";
-export { default as de1970s05 } from "./decada-0070-0005-min.jpg";
-export { default as de1970s06 } from "./decada-0070-0006-min.jpg";
-export { default as de1970s07 } from "./decada-0070-0007-min.jpg";
-export { default as de1970s08 } from "./decada-0070-0008-min.jpg";
-export { default as de1970s09 } from "./decada-0070-0009-min.jpg";
-export { default as de1970s10 } from "./decada-0070-0010-min.jpg";
-export { default as de1980s01 } from "./decada-0080-0001-min.jpg";
-export { default as de1980s02 } from "./decada-0080-0002-min.jpg";
-export { default as de1980s03 } from "./decada-0080-0003-min.jpg";
-export { default as de1980s04 } from "./decada-0080-0004-min.jpg";
-export { default as de1980s05 } from "./decada-0080-0005-min.jpg";
-export { default as de1980s06 } from "./decada-0080-0006-min.jpg";
-export { default as de1980s07 } from "./decada-0080-0007-min.jpg";
-export { default as de1980s08 } from "./decada-0080-0008-min.jpg";
-export { default as de1980s09 } from "./decada-0080-0009-min.jpg";
-export { default as de1980s10 } from "./decada-0080-0010-min.jpg";
-export { default as de1990s01 } from "./decada-0090-0001-min.jpg";
-export { default as de1990s02 } from "./decada-0090-0002-min.jpg";
-export { default as de1990s03 } from "./decada-0090-0003-min.jpg";
-export { default as de1990s04 } from "./decada-0090-0004-min.jpg";
-export { default as de1990s05 } from "./decada-0090-0005-min.jpg";
-export { default as de1990s06 } from "./decada-0090-0006-min.jpg";
-export { default as de1990s07 } from "./decada-0090-0007-min.jpg";
-export { default as de1990s08 } from "./decada-0090-0008-min.jpg";
-export { default as de1990s09 } from "./decada-0090-0009-min.jpeg";
-export { default as de1990s10 } from "./decada-0090-0010-min.jpg";
-export { default as de2000s01 } from "./decada-2000-0001-min.jpg";
-export { default as de2000s02 } from "./decada-2000-0002-min.jpg";
-export { default as de2000s03 } from "./decada-2000-0003-min.jpg";
-export { default as de2000s04 } from "./decada-2000-0004-min.jpg";
-export { default as de2000s05 } from "./decada-2000-0005-min.jpg";
-export { default as de2000s06 } from "./decada-2000-0006-min.jpg";
-export { default as de2000s07 } from "./decada-2000-0007-min.jpg";
-export { default as de2000s08 } from "./decada-2000-0008-min.jpg";
-export { default as de2000s09 } from "./decada-2000-0009-min.jpg";
-export { default as de2000s10 } from "./decada-2000-0010-min.jpeg";
-export { default as de2010s01 } from "./decada-2010-0001-min.jpg";
-export { default as de2010s02 } from "./decada-2010-0002-min.jpg";
-export { default as de2010s03 } from "./decada-2010-0003-min.jpg";
-export { default as de2010s04 } from "./decada-2010-0004-min.jpg";
-export { default as de2010s05 } from "./decada-2010-0005-min.jpg";
-export { default as de2010s06 } from "./decada-2010-0006-min.jpg";
-export { default as de2010s07 } from "./decada-2010-0007-min.jpg";
-export { default as de2010s08 } from "./decada-2010-0008-min.jpg";
-export { default as de2010s09 } from "./decada-2010-0009-min.jpg";
-export { default as pr1960s01 } from "./professor-aristoteles-calazans-simoes.jpg";
-export { default as pr1970s01 } from "./professor-nabuco-lopes-t-da-costa-santos.jpg";
-export { default as pr1970s02 } from "./professor-manuel-machado-r-de-azevedo.jpg";
-export { default as pr1980s01 } from "./professor-joao-ferreira-azevedo.jpg";
-export { default as pr1980s02 } from "./professor-fernando-cardoso-gama.jpg";
-export { default as pr1980s03 } from "./professora-delza-leite-goes-gitai.jpg";
-export { default as pr1990s01 } from "./professor-fernando-cardoso-gama.jpg";
-export { default as pr1990s02 } from "./professor-rogerio-moura-pinheiro.jpg";
-export { default as pr2000s01 } from "./professora-ana-dayse-rezende-dorea.jpg";
-export { default as pr2010s01 } from "./professor-eurico-de-barros-lobo-filho.jpg";
-export { default as pr2010s02 } from "./professora-maria-valéria-costa-correia.jpg";
-export { default as pr2010s03 } from "./professor-josealdo-tonholo.jpg";
+import de1960s01 from "./decada-0060-0001-min.jpg";
+import de1960s02 from "./decada-0060-0002-min.jpg";
+import de1960s03 from "./decada-0060-0003-min.jpg";
+import de1960s04 from "./decada-0060-0004-min.jpg";
+import de1960s05 from "./decada-0060-0005-min.jpg";
+import de1960s06 from "./decada-0060-0006-min.jpg";
+import de1960s07 from "./decada-0060-0007-min.jpg";
+import de1960s08 from "./decada-0060-0008-min.jpg";
+import de1960s09 from "./decada-0060-0009-min.jpg";
+import de1960s10 from "./decada-0060-0010-min.jpg";
+import de1970s01 from "./decada-0070-0001-min.jpg";
+import de1970s02 from "./decada-0070-0002-min.jpg";
+import de1970s03 from "./decada-0070-0003-min.jpg";
+import de1970s04 from "./decada-0070-0004-min.jpg";
+import de1970s05 from "./decada-0070-0005-min.jpg";
+import de1970s06 from "./decada-0070-0006-min.jpg";
+import de1970s07 from "./decada-0070-0007-min.jpg";
+import de1970s08 from "./decada-0070-0008-min.jpg";
+import de1970s09 from "./decada-0070-0009-min.jpg";
+import de1970s10 from "./decada-0070-0010-min.jpg";
+import de1980s01 from "./decada-0080-0001-min.jpg";
+import de1980s02 from "./decada-0080-0002-min.jpg";
+import de1980s03 from "./decada-0080-0003-min.jpg";
+import de1980s04 from "./decada-0080-0004-min.jpg";
+import de1980s05 from "./decada-0080-0005-min.jpg";
+import de1980s06 from "./decada-0080-0006-min.jpg";
+import de1980s07 from "./decada-0080-0007-min.jpg";
+import de1980s08 from "./decada-0080-0008-min.jpg";
+import de1980s09 from "./decada-0080-0009-min.jpg";
+import de1980s10 from "./decada-0080-0010-min.jpg";
+import de1990s01 from "./decada-0090-0001-min.jpg";
+import de1990s02 from "./decada-0090-0002-min.jpg";
+import de1990s03 from "./decada-0090-0003-min.jpg";
+import de1990s04 from "./decada-0090-0004-min.jpg";
+import de1990s05 from "./decada-0090-0005-min.jpg";
+import de1990s06 from "./decada-0090-0006-min.jpg";
+import de1990s07 from "./decada-0090-0007-min.jpg";
+import de1990s08 from "./decada-0090-0008-min.jpg";
+import de1990s09 from "./decada-0090-0009-min.jpeg";
+import de1990s10 from "./decada-0090-0010-min.jpg";
+import de2000s01 from "./decada-2000-0001-min.jpg";
+import de2000s02 from "./decada-2000-0002-min.jpg";
+import de2000s03 from "./decada-2000-0003-min.jpg";
+import de2000s04 from "./decada-2000-0004-min.jpg";
+import de2000s05 from "./decada-2000-0005-min.jpg";
+import de2000s06 from "./decada-2000-0006-min.jpg";
+import de2000s07 from "./decada-2000-0007-min.jpg";
+import de2000s08 from "./decada-2000-0008-min.jpg";
+import de2000s09 from "./decada-2000-0009-min.jpg";
+import de2000s10 from "./decada-2000-0010-min.jpeg";
+import de2010s01 from "./decada-2010-0001-min.jpg";
+import de2010s02 from "./decada-2010-0002-min.jpg";
+import de2010s03 from "./decada-2010-0003-min.jpg";
+import de2010s04 from "./decada-2010-0004-min.jpg";
+import de2010s05 from "./decada-2010-0005-min.jpg";
+import de2010s06 from "./decada-2010-0006-min.jpg";
+import de2010s07 from "./decada-2010-0007-min.jpg";
+import de2010s08 from "./decada-2010-0008-min.jpg";
+import de2010s09 from "./decada-2010-0009-min.jpg";
+import pr1960s01 from "./professor-aristoteles-calazans-simoes.jpg";
+import pr1970s01 from "./professor-nabuco-lopes-t-da-costa-santos.jpg";
+import pr1970s02 from "./professor-manuel-machado-r-de-azevedo.jpg";
+import pr1980s01 from "./professor-joao-ferreira-azevedo.jpg";
+import pr1980s02 from "./professor-fernando-cardoso-gama.jpg";
+import pr1980s03 from "./professora-delza-leite-goes-gitai.jpg";
+import pr1990s01 from "./professor-fernando-cardoso-gama.jpg";
+import pr1990s02 from "./professor-rogerio-moura-pinheiro.jpg";
+import pr2000s01 from "./professora-ana-dayse-rezende-dorea.jpg";
+import pr2010s01 from "./professor-eurico-de-barros-lobo-filho.jpg";
+import pr2010s02 from "./professora-maria-valéria-costa-correia.jpg";
+import pr2010s03 from "./professor-josealdo-tonholo.jpg";
+
+export default Object.assign(
+  {},
+  {
+    de1960s01,
+    de1960s02,
+    de1960s03,
+    de1960s04,
+    de1960s05,
+    de1960s06,
+    de1960s07,
+    de1960s08,
+    de1960s09,
+    de1960s10,
+    de1970s01,
+    de1970s02,
+    de1970s03,
+    de1970s04,
+    de1970s05,
+    de1970s06,
+    de1970s07,
+    de1970s08,
+    de1970s09,
+    de1970s10,
+    de1980s01,
+    de1980s02,
+    de1980s03,
+    de1980s04,
+    de1980s05,
+    de1980s06,
+    de1980s07,
+    de1980s08,
+    de1980s09,
+    de1980s10,
+    de1990s01,
+    de1990s02,
+    de1990s03,
+    de1990s04,
+    de1990s05,
+    de1990s06,
+    de1990s07,
+    de1990s08,
+    de1990s09,
+    de1990s10,
+    de2000s01,
+    de2000s02,
+    de2000s03,
+    de2000s04,
+    de2000s05,
+    de2000s06,
+    de2000s07,
+    de2000s08,
+    de2000s09,
+    de2000s10,
+    de2010s01,
+    de2010s02,
+    de2010s03,
+    de2010s04,
+    de2010s05,
+    de2010s06,
+    de2010s07,
+    de2010s08,
+    de2010s09,
+    pr1960s01,
+    pr1970s01,
+    pr1970s02,
+    pr1980s01,
+    pr1980s02,
+    pr1980s03,
+    pr1990s01,
+    pr1990s02,
+    pr2000s01,
+    pr2010s01,
+    pr2010s02,
+    pr2010s03,
+  },
+);
