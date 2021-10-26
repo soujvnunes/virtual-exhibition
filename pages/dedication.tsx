75fe95cb1bdea668d718ea8b9f48e2ec9ce7ec63
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-26 15:23:24 -0300

    Use `Main` at `Pages`

---
+++ pages/dedication.tsx
@@ -0,0 +1,10 @@
+import { Main } from "containers";
+import Link from "next/link";
+
+export default function Dedication() {
+  return (
+    <Main title="Dedication">
+      teste <Link href="/">home</Link>
+    </Main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import { Button, Layout } from "ui";
+import { Button } from "ui";
+import Link from "next/link";
+import { Main } from "containers";
 
 export default function Home() {
   return (
-    <Layout title="UFAL 60 Anos">
+    <Main title="UFAL 60 Anos">
+      <Link href="/dedication">dedication</Link>
       <Button>test</Button>
-    </Layout>
+    </Main>
   );
 }
