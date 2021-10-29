1f4dcc534c6ac32b6ac16cf85f5f2b3c03a686d1
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-28 22:57:43 -0300

    Live of new feature usages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import { Button, Typography } from "ui";
-import Link from "next/link";
 import { Main } from "containers";
 
 export default function Home() {
   return (
     <Main title="UFAL 60 Anos">
-      <Link href="/dedication">dedication</Link>
-      <Button>test</Button>
+      <Typography href="/dedication" variant="p" color="text">
+        dedication
+      </Typography>
+      {["main", "accent", "error", "warning", "text"].map((color) => (
+        <Button
+          key={color}
+          color={color}
+          base={
+            { main: "bottom", accent: "left", error: "top", warning: "right" }[
+              color
+            ]
+          }
+        >
+          {color}
+        </Button>
+      ))}
       {["h2", "h3", "p", "span"].map((variant) => (
         <Typography
           key={variant}
