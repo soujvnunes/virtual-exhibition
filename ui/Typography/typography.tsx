e51e38ab3db83b53786e59d80996d4bb36d37321
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-28 22:12:42 -0300

    Add capability on `Typography` for handle `button` element

--- ui/Typography/typography.tsx
+++ ui/Typography/typography.tsx
@@ -1,5 +1,6 @@
 import cn from "classnames";
 import { forwardRef } from "react";
+import { isProp } from "utils";
 import { TypographyProps, TypographyRef } from "./typography.t";
 
 const Typography = forwardRef<TypographyRef, TypographyProps>(
@@ -8,22 +9,29 @@ const Typography = forwardRef<TypographyRef, TypographyProps>(
     ref,
   ) {
     const ElementType = element || variant;
+    const isButton = element === "button";
+    const isHeading = isProp(variant, ["h2", "h3"]);
 
     return (
       <ElementType
         ref={ref}
         className={cn(
+          "transition-colors",
           {
             "text-075": variant === "span",
             "text-100": variant === "p",
-            "text-200 md:text-300 xl:text-400 capitalize font-bold":
-              variant === "h3",
-            "text-225 md:text-325 xl:text-425 capitalize font-bold":
-              variant === "h2",
-            "font-bold": weight === "bold",
-            "opacity-100": state === "focused",
-            "opacity-20": state === "disabled",
-            "opacity-60": !state,
+            "text-200 md:text-300 xl:text-400": variant === "h3",
+            "text-225 md:text-325 xl:text-425": variant === "h2",
+          },
+          {
+            "font-bold": weight === "bold" || isButton || isHeading,
+          },
+          {
+            "text-opacity-100": state === "focused",
+            "text-opacity-20": state === "disabled",
+            "text-opacity-60": !state,
+          },
+          {
             "text-main": color === "main",
             "text-accent": color === "accent",
             "text-success": color === "success",
@@ -31,6 +39,10 @@ const Typography = forwardRef<TypographyRef, TypographyProps>(
             "text-error": color === "error",
             "text-black dark:text-white": color === "text",
             "text-inherit": !color,
+          },
+          {
+            "hover:text-opacity-100 uppercase": isButton,
+            "uppercase tracking-widest": isButton || isHeading,
           },
           className,
         )}
