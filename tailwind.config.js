7cac3b9e97253da036948094eb45dbf5644e74f4
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-28 22:11:43 -0300

    Enable `:active` variants for some css tailwind utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,10 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+      backgroundOpacity: ["active"],
+    },
   },
   plugins: [],
 };
