45547c11c2c6fc4fc005ae389d57a89dbbd042c2
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-28 22:13:09 -0300

    Build `Button`  component with `Typography` one

--- ui/Button/button.t.ts
+++ ui/Button/button.t.ts
@@ -1,4 +1,7 @@
 import { ComponentPropsWithRef } from "react";
+import { Typography } from "ui";
 
 export interface ButtonRef extends HTMLButtonElement {}
-export interface ButtonProps extends ComponentPropsWithRef<"button"> {}
+export interface ButtonProps extends ComponentPropsWithRef<typeof Typography> {
+  base?: "top" | "right" | "bottom" | "left";
+}
--- ui/Button/button.tsx
+++ ui/Button/button.tsx
@@ -1,17 +1,60 @@
 import cn from "classnames";
 import { forwardRef } from "react";
+import { Typography } from "ui";
+import { isProp } from "utils";
 import { ButtonProps, ButtonRef } from "./button.t";
 
 const Button = forwardRef<ButtonRef, ButtonProps>(function Button(
-  { className, ...props },
+  { className, children, base = "bottom", ...props },
   ref,
 ) {
+  const { color } = props;
+  const isBaseX = isProp(base, ["bottom", "top"]);
+  const isBaseY = isProp(base, ["bottom", "left"]);
+  const rounded = {
+    "rounded-b": base === "top",
+    "rounded-l": base === "right",
+    "rounded-t": base === "bottom",
+    "rounded-r": base === "left",
+  };
+
   return (
-    <button
+    <Typography
       ref={ref}
-      className={cn("relative max-w-min", className)}
+      element="button"
+      variant="p"
+      className={cn(
+        "relative max-w-min h-12 px-4 bg-opacity-0 active:bg-opacity-20 rounded-t",
+        {
+          "bg-main": color === "main",
+          "bg-accent": color === "accent",
+          "bg-success": color === "success",
+          "bg-warning": color === "warning",
+          "bg-error": color === "error",
+          "bg-black dark:bg-white": color === "text",
+          "bg-inherit": !color,
+        },
+        rounded,
+        className,
+      )}
       {...props}
-    />
+    >
+      {children}
+      <div
+        className={cn(
+          "absolute bg-current",
+          {
+            "w-full h-1": isBaseX,
+            "h-full w-1": !isBaseX,
+          },
+          {
+            "bottom-0 left-0": isBaseY,
+            "top-0 right-0": !isBaseY,
+          },
+          rounded,
+        )}
+      />
+    </Typography>
   );
 });
 
