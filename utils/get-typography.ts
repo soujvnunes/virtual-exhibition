14108de00e122b966697e3c7bf52859f7e3f2485
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-10-31 16:17:00 -0300

    Create typography class names utility

---
+++ utils/get-typography.ts
@@ -0,0 +1,62 @@
+import cn from "classnames";
+import { ElementType } from "react";
+import isProp from "./is-prop";
+
+export interface getTypographyProps {
+  element?: ElementType;
+  variant?: "h2" | "h3" | "p" | "span";
+  weight?: "bold";
+  state?: "focused" | "disabled";
+  color?: "main" | "accent" | "success" | "warning" | "error" | "text";
+}
+
+export default function getTypography({
+  element,
+  variant,
+  weight,
+  state,
+  color,
+}: getTypographyProps): string {
+  const isHeading = isProp(variant, ["h2", "h3"]);
+  const isSvg = element === "svg";
+  const isButton = element === "button";
+  const isLink = element === "a";
+
+  return cn(
+    {
+      "text-075": variant === "span",
+      "text-100": variant === "p",
+      "text-200 md:text-300 xl:text-400": variant === "h3",
+      "text-225 md:text-325 xl:text-425": variant === "h2",
+    },
+    {
+      "font-bold": weight === "bold",
+    },
+    {
+      "text-opacity-100": state === "focused",
+      "text-opacity-20": state === "disabled",
+      "text-opacity-60": !state,
+    },
+    {
+      "text-main": color === "main",
+      "text-accent": color === "accent",
+      "text-success": color === "success",
+      "text-warning": color === "warning",
+      "text-error": color === "error",
+      "text-black dark:text-white": color === "text",
+      "text-inherit": !color && !isSvg,
+      "fill-current": !color && isSvg,
+    },
+    {
+      underline: isLink,
+    },
+    {
+      "uppercase tracking-widest": isHeading,
+    },
+    {
+      "hover:text-opacity-100 focus:text-opacity-100 outline-none":
+        isLink || isButton,
+      "uppercase tracking-widest": isButton || isHeading,
+    },
+  );
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export { default as getExhibition } from "./get-exhibition";
 export { default as getTributes } from "./get-tributes";
+export { default as getTypography } from "./get-typography";
 export { default as isProp } from "./is-prop";
