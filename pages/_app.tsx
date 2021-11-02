606b25781b741fc082ff1fb01ba5c4f7a06c47d7
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-11-02 13:27:26 -0300

    Use `Main` directly on the pages root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { Exhibition, Header } from "containers";
 import "index.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const title = "Ufal 60 Anos";
+
   return (
     <>
       <Head>
@@ -12,8 +15,18 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
+        <title>{title}</title>
       </Head>
-      <Component {...pageProps} />
+      <main
+        title={title}
+        className={
+          "flex flex-col min-h-screen mx-auto px-4 w-full sm:px-5 md:max-w-screen-sm md:px-6 lg:max-w-screen-md lg:px-7 xl:max-w-screen-lg xl:px-8"
+        }
+      >
+        <Header />
+        <Exhibition />
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
--- pages/dedication.tsx
+++ pages/dedication.tsx
@@ -1,10 +1,9 @@
-import { Main } from "containers";
 import Link from "next/link";
 
 export default function Dedication() {
   return (
-    <Main title="Dedication">
+    <>
       teste <Link href="/">home</Link>
-    </Main>
+    </>
   );
 }
