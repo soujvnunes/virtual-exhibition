a6d8e3a42e0d35a01387b52c4917657146603017
Victor Nunes <16447765+soujvnunes@users.noreply.github.com>
2021-11-02 21:08:27 -0300

    Replace `Ufal 60 anos` with `Exposição Virtual da UFAL`

--- containers/header.tsx
+++ containers/header.tsx
@@ -3,7 +3,7 @@ import { Button, Icon, Typography } from "ui";
 export default function Header() {
   return (
     <header className="flex justify-center">
-      <Typography variant="h1">UFAL 60 Anos</Typography>
+      <Typography variant="h1">Exposição Virtual da UFAL</Typography>
       <Button base="top" color="text" adornmentEnd={<Icon>menu</Icon>}>
         Exposição
       </Button>
