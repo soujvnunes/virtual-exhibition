f315aaee980348a8b516fea745fc2dd67b0f3dae
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-03-13 21:03:28 -0300

    Test custom diffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-const App = () => <div>oi</div>;
+const App = () => (
+  <>
+    <p className="text-md">
+      Fazer a diferença para o nosso povo. A medida da grandeza de uma
+      instituição está em seu potencial de transformar realidades e este
+      potencial desenvolvemos, ao longo das últimas seis décadas, com maestria.
+      Colocamos nossos valores na formação de cada estudante que ingressa nos
+      nossos cursos e trabalhamos exaustivamente para sermos motivo de orgulho
+      para nossa gente.
+    </p>
+    <h1 className="font-serif text-x3l md:text-x4l lg:text-x5l">
+      A Universidade Federal de Alagoas completa 60 anos com uma história
+      enraizada na vida do povo alagoano.
+    </h1>
+  </>
+);
 
 export default App;
