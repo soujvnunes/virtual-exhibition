23fc725e41021cc2ed48a0d5f4e8af49cc68c250
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-13 12:35:22 -0300

    Create `Button`

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Container } from "./Container";
+export { default as Button } from "./Button";
 export * from "./Icon";
 export { default as Logos } from "./Logos";
 export { default as Text } from "./Text";
