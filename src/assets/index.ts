96ae5de0ec67c69125edabd37961bedcdd6b1b89
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-13 20:00:38 -0300

    Set up `styled-components`

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,145 +1,71 @@
-import d1601 from "./d-1-6-1.jpg";
-import d1602 from "./d-1-6-2.jpg";
-import d1603 from "./d-1-6-3.jpg";
-import d1604 from "./d-1-6-4.jpg";
-import d1605 from "./d-1-6-5.jpg";
-import d1606 from "./d-1-6-6.jpg";
-import d1607 from "./d-1-6-7.jpg";
-import d1608 from "./d-1-6-8.jpg";
-import d1609 from "./d-1-6-9.jpg";
-import d1610 from "./d-1-6-10.jpg";
-import d1701 from "./d-1-7-1.jpg";
-import d1702 from "./d-1-7-2.jpg";
-import d1703 from "./d-1-7-3.jpg";
-import d1704 from "./d-1-7-4.jpg";
-import d1705 from "./d-1-7-5.jpg";
-import d1706 from "./d-1-7-6.jpg";
-import d1707 from "./d-1-7-7.jpg";
-import d1708 from "./d-1-7-8.jpg";
-import d1709 from "./d-1-7-9.jpg";
-import d1710 from "./d-1-7-10.jpg";
-import d1801 from "./d-1-8-1.jpg";
-import d1802 from "./d-1-8-2.jpg";
-import d1803 from "./d-1-8-3.jpg";
-import d1804 from "./d-1-8-4.jpg";
-import d1805 from "./d-1-8-5.jpg";
-import d1806 from "./d-1-8-6.jpg";
-import d1807 from "./d-1-8-7.jpg";
-import d1808 from "./d-1-8-8.jpg";
-import d1809 from "./d-1-8-9.jpg";
-import d1810 from "./d-1-8-10.jpg";
-import d1901 from "./d-1-9-1.jpg";
-import d1902 from "./d-1-9-2.jpg";
-import d1903 from "./d-1-9-3.jpg";
-import d1904 from "./d-1-9-4.jpg";
-import d1905 from "./d-1-9-5.jpg";
-import d1906 from "./d-1-9-6.jpg";
-import d1907 from "./d-1-9-7.jpg";
-import d1908 from "./d-1-9-8.jpg";
-import d1909 from "./d-1-9-9.jpeg";
-import d1910 from "./d-1-9-10.jpg";
-import d2001 from "./d-2-0-1.jpg";
-import d2002 from "./d-2-0-2.jpg";
-import d2003 from "./d-2-0-3.jpg";
-import d2004 from "./d-2-0-4.jpg";
-import d2005 from "./d-2-0-5.jpg";
-import d2006 from "./d-2-0-6.jpg";
-import d2007 from "./d-2-0-7.jpg";
-import d2008 from "./d-2-0-8.jpg";
-import d2009 from "./d-2-0-9.jpg";
-import d2010 from "./d-2-0-10.jpeg";
-import d2101 from "./d-2-1-1.jpg";
-import d2102 from "./d-2-1-2.jpg";
-import d2103 from "./d-2-1-3.jpg";
-import d2104 from "./d-2-1-4.jpg";
-import d2105 from "./d-2-1-5.jpg";
-import d2106 from "./d-2-1-6.jpg";
-import d2107 from "./d-2-1-7.jpg";
-import d2108 from "./d-2-1-8.jpg";
-import d2109 from "./d-2-1-9.jpg";
-import p1601 from "./p-aristoteles-simoes.jpg";
-import p1701 from "./p-nabuco-santos.jpg";
-import p1702 from "./p-manuel-azevedo.jpg";
-import p1801 from "./p-joao-azevedo.jpg";
-import p1802 from "./p-fernando-gama.jpg";
-import p1803 from "./p-delza-gitai.jpg";
-import p1901 from "./p-fernando-gama.jpg";
-import p1902 from "./p-rogerio-pinheiro.jpg";
-import p2001 from "./p-ana-dorea.jpg";
-import p2101 from "./p-eurico-filho.jpg";
-import p2102 from "./p-maria-correia.jpg";
-import p2103 from "./p-josealdo-tonholo.jpg";
-
-export default {
-  d1601,
-  d1602,
-  d1603,
-  d1604,
-  d1605,
-  d1606,
-  d1607,
-  d1608,
-  d1609,
-  d1610,
-  d1701,
-  d1702,
-  d1703,
-  d1704,
-  d1705,
-  d1706,
-  d1707,
-  d1708,
-  d1709,
-  d1710,
-  d1801,
-  d1802,
-  d1803,
-  d1804,
-  d1805,
-  d1806,
-  d1807,
-  d1808,
-  d1809,
-  d1810,
-  d1901,
-  d1902,
-  d1903,
-  d1904,
-  d1905,
-  d1906,
-  d1907,
-  d1908,
-  d1909,
-  d1910,
-  d2001,
-  d2002,
-  d2003,
-  d2004,
-  d2005,
-  d2006,
-  d2007,
-  d2008,
-  d2009,
-  d2010,
-  d2101,
-  d2102,
-  d2103,
-  d2104,
-  d2105,
-  d2106,
-  d2107,
-  d2108,
-  d2109,
-  p1601,
-  p1701,
-  p1702,
-  p1801,
-  p1802,
-  p1803,
-  p1901,
-  p1902,
-  p2001,
-  p2101,
-  p2102,
-  p2103,
-};
+export { default as d1601 } from "./d-1-6-1.jpg";
+export { default as d1602 } from "./d-1-6-2.jpg";
+export { default as d1603 } from "./d-1-6-3.jpg";
+export { default as d1604 } from "./d-1-6-4.jpg";
+export { default as d1605 } from "./d-1-6-5.jpg";
+export { default as d1606 } from "./d-1-6-6.jpg";
+export { default as d1607 } from "./d-1-6-7.jpg";
+export { default as d1608 } from "./d-1-6-8.jpg";
+export { default as d1609 } from "./d-1-6-9.jpg";
+export { default as d1610 } from "./d-1-6-10.jpg";
+export { default as d1701 } from "./d-1-7-1.jpg";
+export { default as d1702 } from "./d-1-7-2.jpg";
+export { default as d1703 } from "./d-1-7-3.jpg";
+export { default as d1704 } from "./d-1-7-4.jpg";
+export { default as d1705 } from "./d-1-7-5.jpg";
+export { default as d1706 } from "./d-1-7-6.jpg";
+export { default as d1707 } from "./d-1-7-7.jpg";
+export { default as d1708 } from "./d-1-7-8.jpg";
+export { default as d1709 } from "./d-1-7-9.jpg";
+export { default as d1710 } from "./d-1-7-10.jpg";
+export { default as d1801 } from "./d-1-8-1.jpg";
+export { default as d1802 } from "./d-1-8-2.jpg";
+export { default as d1803 } from "./d-1-8-3.jpg";
+export { default as d1804 } from "./d-1-8-4.jpg";
+export { default as d1805 } from "./d-1-8-5.jpg";
+export { default as d1806 } from "./d-1-8-6.jpg";
+export { default as d1807 } from "./d-1-8-7.jpg";
+export { default as d1808 } from "./d-1-8-8.jpg";
+export { default as d1809 } from "./d-1-8-9.jpg";
+export { default as d1810 } from "./d-1-8-10.jpg";
+export { default as d1901 } from "./d-1-9-1.jpg";
+export { default as d1902 } from "./d-1-9-2.jpg";
+export { default as d1903 } from "./d-1-9-3.jpg";
+export { default as d1904 } from "./d-1-9-4.jpg";
+export { default as d1905 } from "./d-1-9-5.jpg";
+export { default as d1906 } from "./d-1-9-6.jpg";
+export { default as d1907 } from "./d-1-9-7.jpg";
+export { default as d1908 } from "./d-1-9-8.jpg";
+export { default as d1909 } from "./d-1-9-9.jpeg";
+export { default as d1910 } from "./d-1-9-10.jpg";
+export { default as d2001 } from "./d-2-0-1.jpg";
+export { default as d2002 } from "./d-2-0-2.jpg";
+export { default as d2003 } from "./d-2-0-3.jpg";
+export { default as d2004 } from "./d-2-0-4.jpg";
+export { default as d2005 } from "./d-2-0-5.jpg";
+export { default as d2006 } from "./d-2-0-6.jpg";
+export { default as d2007 } from "./d-2-0-7.jpg";
+export { default as d2008 } from "./d-2-0-8.jpg";
+export { default as d2009 } from "./d-2-0-9.jpg";
+export { default as d2010 } from "./d-2-0-10.jpeg";
+export { default as d2101 } from "./d-2-1-1.jpg";
+export { default as d2102 } from "./d-2-1-2.jpg";
+export { default as d2103 } from "./d-2-1-3.jpg";
+export { default as d2104 } from "./d-2-1-4.jpg";
+export { default as d2105 } from "./d-2-1-5.jpg";
+export { default as d2106 } from "./d-2-1-6.jpg";
+export { default as d2107 } from "./d-2-1-7.jpg";
+export { default as d2108 } from "./d-2-1-8.jpg";
+export { default as d2109 } from "./d-2-1-9.jpg";
+export { default as p1601 } from "./p-aristoteles-simoes.jpg";
+export { default as p1701 } from "./p-nabuco-santos.jpg";
+export { default as p1702 } from "./p-manuel-azevedo.jpg";
+export { default as p1801 } from "./p-joao-azevedo.jpg";
+export { default as p1802 } from "./p-fernando-gama.jpg";
+export { default as p1803 } from "./p-delza-gitai.jpg";
+export { default as p1901 } from "./p-fernando-gama.jpg";
+export { default as p1902 } from "./p-rogerio-pinheiro.jpg";
+export { default as p2001 } from "./p-ana-dorea.jpg";
+export { default as p2101 } from "./p-eurico-filho.jpg";
+export { default as p2102 } from "./p-maria-correia.jpg";
+export { default as p2103 } from "./p-josealdo-tonholo.jpg";
