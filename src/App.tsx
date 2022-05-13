78f23bd49916cc04493f7938ef6687564868b172
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-13 12:35:40 -0300

    Use `Button`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, IconA11y, IconExit, Logos, Text } from "ui";
+import { Button, Container, IconA11y, IconExit, Logos, Text } from "ui";
 
 function App() {
   return (
@@ -8,34 +8,33 @@ function App() {
         <button className="p-xs border border-main/40 rounded ml-auto text-main-light">
           <IconA11y />
         </button>
-        <button className="py-xs pr-sm pl-lg border border-main/40 rounded ml-sm text-main-light flex text-sm items-center uppercase font-bold tracking-wider">
-          Notícias
-          <IconExit className="ml-sm" />
-        </button>
+        <Button icon={<IconExit />}>Notícias</Button>
       </Container>
-      {(["special", "main", "success", "warning", "error"] as const).map(
-        (color) => (
-          <Text key={color} variant="headline">
-            This is the <Text color={color}>{color}</Text> color.
+      <Container>
+        {(["special", "main", "success", "warning", "error"] as const).map(
+          (color) => (
+            <Text key={color} variant="headline">
+              This is the <Text color={color}>{color}</Text> color.
+            </Text>
+          ),
+        )}
+        {(
+          [
+            "normal",
+            "normalItalic",
+            "medium",
+            "mediumItalic",
+            "semibold",
+            "semiboldItalic",
+            "bold",
+            "boldItalic",
+          ] as const
+        ).map((variant) => (
+          <Text key={variant} as="p" weight={variant}>
+            This is the {variant} variant.
           </Text>
-        ),
-      )}
-      {(
-        [
-          "normal",
-          "normalItalic",
-          "medium",
-          "mediumItalic",
-          "semibold",
-          "semiboldItalic",
-          "bold",
-          "boldItalic",
-        ] as const
-      ).map((variant) => (
-        <Text key={variant} as="p" weight={variant}>
-          This is the {variant} variant.
-        </Text>
-      ))}
+        ))}
+      </Container>
     </>
   );
 }
