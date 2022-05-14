0c8da7d0a55c0f9e139dca49413a3f5ed96d9a88
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-14 19:44:27 -0300

    Moving constants to its proper directory

---
+++ src/consts/index.ts
@@ -0,0 +1,4 @@
+export { default as FONT_WEIGHTS } from "./fontWeights";
+export { default as INITIAL_THEME } from "./initialTheme";
+export { default as TEXT_ALIGNS } from "./textAligns";
+export { default as TEXT_VARIANTS } from "./textVariants";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,5 @@
-export { default as FONT_WEIGHTS } from "./fontWeights";
 export { default as getExhibition } from "./getExhibition";
 export { default as getHomages } from "./getHomages";
 export { default as isProp } from "./isProp";
 export { default as reportWebVitals } from "./reportWebVitals";
-export { default as TEXT_ALIGNS } from "./textAligns";
-export { default as TEXT_VARIANTS } from "./textVariants";
 export { default as withSvgStyles } from "./withSvgStyle";
