8712bfd2562f18e9a30dc87799753ae73544d68e
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-14 19:48:30 -0300

    Simplify `Theme` as `GlobalStyle`

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,7 +1,6 @@
-export { default as Container } from "./Container/Container";
+export { default as Container } from "./Container";
+export { default as GlobalStyle } from "./GlobalStyle";
 export { default as Button } from "./Button";
 export * from "./Icon";
 export { default as Logos } from "./Logos";
 export { default as Text } from "./Text";
-export { default as Theme } from "./Theme";
-export * from "./Theme";
