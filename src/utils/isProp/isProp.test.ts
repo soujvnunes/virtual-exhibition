109d99e8c1e95c543207376b760bfeb36ee60365
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-22 18:37:16 -0300

    Resume `isProp()` tests

--- src/utils/isProp/isProp.test.ts
+++ src/utils/isProp/isProp.test.ts
@@ -1,60 +1,16 @@
 import isProp from "./isProp";
 
 describe("isProp", () => {
-  describe("with strings", () => {
-    it("should be truthy when at least one of the values is equal to the prop", () => {
-      expect(isProp("a", ["a", "b"])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to the prop", () => {
-      expect(isProp("a", ["b", "c"])).toBe(false);
-    });
-    it("should be truthy when at least one of the values is equal to one of the props", () => {
-      expect(isProp(["a", "b"], ["a", "c"])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to any prop", () => {
-      expect(isProp(["a", "b"], ["c", "d"])).toBe(false);
-    });
+  it("should be truthy when at least one of the values is equal to the prop", () => {
+    expect(isProp("a", ["a", "b"])).toBeTruthy();
   });
-  describe("with string and numbers", () => {
-    it("should be truthy when at least one of the values is equal to the prop", () => {
-      expect(isProp(1, [1, "b"])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to the prop", () => {
-      expect(isProp(1, ["b", 2])).toBe(false);
-    });
-    it("should be truthy when at least one of the values is equal to one of the props", () => {
-      expect(isProp([1, 2], ["a", 2])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to any prop", () => {
-      expect(isProp([1, "b"], ["c", 2])).toBe(false);
-    });
+  it("should be falsy when any value isn't equal to the prop", () => {
+    expect(isProp(1, ["b", 2])).toBeFalsy();
   });
-  describe("with numbers and booleans", () => {
-    it("should be truthy when at least one of the values is equal to the prop", () => {
-      expect(isProp(false, [true, false])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to the prop", () => {
-      expect(isProp(false, [true, 0])).toBe(false);
-    });
-    it("should be truthy when at least one of the values is equal to one of the props", () => {
-      expect(isProp([true, 2], ["a", 2])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to any prop", () => {
-      expect(isProp([1, false], [true, 2])).toBe(false);
-    });
+  it("should be truthy when at least one of the values is equal to one of the props", () => {
+    expect(isProp([true, 2], ["a", 2])).toBeTruthy();
   });
-  describe("with booleans and undefined case", () => {
-    it("should be truthy when at least one of the values is equal to the prop", () => {
-      expect(isProp(undefined, [true, undefined])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to the prop", () => {
-      expect(isProp(undefined, [true, false])).toBe(false);
-    });
-    it("should be truthy when at least one of the values is equal to one of the props", () => {
-      expect(isProp([true, undefined], [undefined, 2])).toBe(true);
-    });
-    it("should be falsy when any value isn't equal to any prop", () => {
-      expect(isProp([undefined, false], [true, true])).toBe(false);
-    });
+  it("should be falsy when any value isn't equal to any prop", () => {
+    expect(isProp([undefined, false], [true, true])).toBeFalsy();
   });
 });
