153565afef8e74586043fd15a10a443daf3352d8
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-21 21:17:39 -0300

    Add util to handle a styled wrapped component

--- src/spec.d.ts
+++ src/spec.d.ts
@@ -1,8 +1,22 @@
+import { ComponentPropsWithRef, ElementType } from "react";
 import "styled-components";
 import { GlobalStyle } from "ui";
 
 type Theme = typeof GlobalStyle.THEME;
+type Component = ElementType;
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
+  export type ComponentPropsWithAs<
+    P extends object,
+    T extends keyof JSX.IntrinsicElements = void,
+  > = T extends keyof JSX.IntrinsicElements
+    ? ComponentPropsWithRef<T> &
+        P & {
+          as?: keyof JSX.IntrinsicElements;
+        }
+    : {
+        $props: P;
+        as?: keyof JSX.IntrinsicElements;
+      };
 }
