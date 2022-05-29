6c69660bbd5fd809962c78fed1c6f3984367ee1c
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-29 12:00:47 -0300

    Test `Container`

--- src/ui/Container/Container.test.tsx
+++ src/ui/Container/Container.test.tsx
@@ -1,8 +1,17 @@
-import { render } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import Container from "./Container";
 
-describe("<Container />", () => {
+afterEach(cleanup);
+
+const testId = "container";
+
+describe("UI.Container", () => {
   test("renders without crash", () => {
-    render(<Container />);
+    render(<Container data-testid={testId} />);
+
+    const container = screen.getByTestId(testId);
+
+    expect(container).toBeInTheDocument();
+    expect(container).toHaveClass(Container.Root.styledComponentId);
   });
 });
