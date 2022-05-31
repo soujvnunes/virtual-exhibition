a6370b4d7ff2eec5be3df570f5e9f27275c4db77
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-31 09:38:06 -0300

    Revamp `Button`

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,6 @@
+export { default as Button } from "./Button";
 export { default as Container } from "./Container";
 export { default as GlobalStyle } from "./GlobalStyle";
-export { default as Button } from "./Button";
 export * from "./Icon";
 export { default as Logos } from "./Logos";
 export { default as Typography } from "./Typography";
