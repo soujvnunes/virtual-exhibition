1a57a45293b1af469b55ddd99221546839039183
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-31 09:37:37 -0300

    Revamp `ComponentPropsWithAs`

--- src/spec.d.ts
+++ src/spec.d.ts
@@ -1,22 +1,16 @@
-import { ComponentPropsWithRef } from "react";
+import { ComponentPropsWithRef, ComponentType } from "react";
 import "styled-components";
 import { GlobalStyle } from "ui";
 
 type Theme = typeof GlobalStyle.THEME;
-type AsC = keyof JSX.IntrinsicElements;
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
-  export type ComponentPropsWithAs<
-    P extends object,
-    T extends keyof JSX.IntrinsicElements = void,
-  > = T extends keyof JSX.IntrinsicElements
-    ? ComponentPropsWithRef<T> &
-        P & {
-          as?: AsC;
-        }
-    : {
-        $props: P;
-        as?: AsC;
-      };
+  export type AsC = { as?: keyof JSX.IntrinsicElements | ComponentType<any> };
+  export type ComponentPropsWithAs<P extends object, T = void> = AsC &
+    (T extends keyof JSX.IntrinsicElements
+      ? ComponentPropsWithRef<T> & P
+      : {
+          $props: P;
+        });
 }
