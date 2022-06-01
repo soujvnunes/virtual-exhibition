fa121cdb08242bf897f8b0f9cd93864098bc50f1
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-06-01 15:33:19 -0300

    Enhance components declaration

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -14,13 +14,7 @@ const WrapStart = styled.span`
 const WrapEnd = styled.span`
   margin-left: ${g("12")};
 `;
-const Button = styled(({ children, ...props }: Props) => (
-  <ButtonBase {...props}>
-    {props.$iconStart && <WrapStart>{props.$iconStart}</WrapStart>}
-    {children}
-    {props.$iconEnd && <WrapEnd>{props.$iconEnd}</WrapEnd>}
-  </ButtonBase>
-))`
+const ButtonRoot = styled(ButtonBase)<Props>`
   padding-top: ${(props) =>
     props.$iconStart || props.$iconEnd ? g("8") : g("12")};
   padding-bottom: ${(props) =>
@@ -29,5 +23,14 @@ const Button = styled(({ children, ...props }: Props) => (
   padding-left: ${(props) => (props.$iconStart ? g("12") : g("24"))};
 `;
 
+function Button({ children, ...props }: Props) {
+  return (
+    <ButtonRoot {...props}>
+      {props.$iconStart && <WrapStart>{props.$iconStart}</WrapStart>}
+      {children}
+      {props.$iconEnd && <WrapEnd>{props.$iconEnd}</WrapEnd>}
+    </ButtonRoot>
+  );
+}
 Button.displayName = "Button";
 export default Button;
