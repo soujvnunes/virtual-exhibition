a0e57a04dc85dd0f3c362194c73fdaad440c60ef
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-05-31 21:22:44 -0300

    Remove unused utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export { default as getExhibition } from "./getExhibition";
 export { default as getTheme } from "./getTheme";
 export { default as getHomages } from "./getHomages";
 export { default as isProp } from "./isProp";
-export { default as withSvgStyles } from "./withSvgStyle";
