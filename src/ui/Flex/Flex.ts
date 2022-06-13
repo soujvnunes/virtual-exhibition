be24d95a5f3bd9f54628b54cddfc27b93d56e83f
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-06-12 22:29:00 -0300

    Add `displayName` for Flex

--- src/ui/Flex/Flex.ts
+++ src/ui/Flex/Flex.ts
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 import { $FlexProps } from "types";
 
+/**
+ * TODO
+ *
+ * add
+ *  $display: flex | inline-flex
+ *
+ * change
+ *  $container
+ *    to set 'flex-direction: row; flex-flow: wrap; width: 100%'
+ *  $item
+ *    to be a object { xs: '1~12', md: '1~12', lg: '1~12' } as
+ *    columns and set 'width: x%; flex: 0 1 x%' according of its columns
+ *
+ */
 const Flex = styled.div<$FlexProps>`
   display: ${(props) => props.$container && "flex"};
   flex-direction: ${(props) => props.$direction};
@@ -14,4 +28,5 @@ const Flex = styled.div<$FlexProps>`
   align-self: ${(props) => props.$self};
 `;
 
+Flex.displayName = "Flex";
 export default Flex;
