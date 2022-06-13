32c78a248072a7fcea7eaa28e085f8f3a26caad1
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-06-13 15:12:22 -0300

    Inherit `Button` text styles from `Text`

--- src/ui/Button/Button.ts
+++ src/ui/Button/Button.ts
@@ -2,20 +2,25 @@ import styled from "styled-components";
 import { token as g } from "utils";
 import { ButtonProps } from "types";
 import Surface from "ui/Surface";
+import Text from "ui/Text";
 
+const ButtonTextStyle = styled(Text).attrs({
+  $variant: "overline",
+  $weight: 700,
+})`
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  line-height: 1.167;
+  text-decoration: ${(props) => props.href && "none"};
+`;
 const Button = styled(Surface).attrs((props: ButtonProps) => ({
   $align: "center",
-  as: props.href ? "a" : "button",
+  as: ButtonTextStyle,
+  forwardedAs: props.href ? "a" : "button",
 }))<ButtonProps>`
   background-color: transparent;
   height: calc(${g("32")} + ${g("8")});
   padding: 0 ${g("24")} 0 ${g("24")};
-  text-transform: uppercase;
-  letter-spacing: 0.1em;
-  font-size: ${g("overline")};
-  font-weight: 700;
-  line-height: 1.167;
-  text-decoration: ${(props) => props.href && "none"};
 
   &:focus {
     outline: 0;
