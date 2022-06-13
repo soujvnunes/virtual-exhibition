e2a46cbfdeef49fd3514b708652efd0435ab4bf6
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-06-12 22:30:55 -0300

    Create `Surface`

---
+++ src/ui/Surface/Surface.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Flex from "ui/Flex";
+import { token as t } from "utils";
+
+const Surface = styled(Flex).attrs({ $container: true })`
+  border: 1px solid rgb(${t("primary")} / ${t("hover")});
+  border-radius: ${t("16")};
+  color: rgb(${t("secondary")});
+`;
+
+Surface.displayName = "Surface";
+export default Surface;
