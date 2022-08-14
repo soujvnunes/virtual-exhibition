71509e075a13a7e9f5c295d3d4575a697aabc16c
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-08-13 21:06:10 -0300

    Adjust type imports

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,5 @@
 import "styled-components";
-import { Theme } from "utils";
+import { Theme } from "app";
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,14 +1,3 @@
-import { theme } from "app";
-
-export type Theme = typeof theme;
-
-export type ThemeMedia = keyof Theme["media"];
-
-export type ThemeMediaQuery = Exclude<
-  ThemeMedia,
-  "light" | "dark" | "idle" | "motion"
->;
-
 export type PropsWithAs<P extends Record<string, unknown>> = {
   as?: keyof JSX.IntrinsicElements | React.ComponentType<P>;
 } & P;
