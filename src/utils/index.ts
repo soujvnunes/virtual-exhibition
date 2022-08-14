d6553e14656cdfeace95b11a375cfe0790692bfc
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-08-13 21:08:52 -0300

    Remove unused utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as createIcon, IconRoot } from "./createIcon";
-export { default as getToken } from "./getToken";
 export { default as isProp } from "./isProp";
 export * from "./types";
