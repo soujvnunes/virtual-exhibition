1a62547ef8598bd6e2c7503741a6b79a435c9d68
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-02 11:26:22 -0300

    feat: revamp usefull units with new `useTheme`

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,11 @@
-import { Header, Intro } from "app";
-import { Theme } from "theme";
-
-const themeFactory = {
-  color: {
-    main: "#ff99ff",
-    success: "#66ff33",
-    info: "#3399ff",
-    warning: "#ffcc00",
-    error: "#ff3333",
-  },
-  contrast: {
-    white: "#fff",
-    black: "#333",
-  },
-  state: {
-    primary: 1,
-    secondary: 0.6,
-    tertiary: 0.4,
-    quaternary: 0.2,
-    quinary: 0.16,
-    senary: 0.09,
-  },
-  tone: {
-    lighter: 0.1,
-    light: 0.4,
-    dark: 0.8,
-    darker: 0.1,
-  },
-  size: [
-    1280, 640, 320, 160, 80, 74, 72, 68, 64, 60, 56, 52, 48, 44, 40, 36, 32, 28,
-    24, 20, 16, 12, 8, 4, 2, 1, 0,
-  ],
-  media: {
-    sm: "@media (min-width: 0rem) or (max-width: 640px)",
-    md: "@media (min-width: 640px)",
-    lg: "@media (min-width: 1280px)",
-    light:
-      "@media (prefers-color-scheme: no-preference) or (prefers-color-scheme: light)",
-    dark: "@media (prefers-color-scheme: dark)",
-    idle: "@media (prefers-reduced-motion: reduce)",
-    motion: "@media (prefers-reduced-motion: no-preference)",
-  },
-};
+import { Header } from "app";
+import "./App.scss";
+import { ThemeProvider } from "utils";
 
 export default function App() {
   return (
-    <Theme factory={themeFactory}>
-      {/* <GlobalStyle />
-       */}
+    <ThemeProvider>
       <Header />
-      <Intro />
-    </Theme>
+    </ThemeProvider>
   );
 }
