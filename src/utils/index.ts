f1d2a0edf746d834e589831c92a342397dc50a27
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-10-02 11:25:35 -0300

    feat: revamp usefull files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
-export { default as createIcon, IconRoot } from "./createIcon";
+export { default as ThemeProvider, useTheme } from "./useTheme";
 export { default as isProp } from "./isProp";
-export * from "./types";
