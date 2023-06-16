fa086084af0465994e5cbc9410e971b6d2114abd
soujvnunes <soujvnunes@gmail.com>
2023-06-16 17:47:37 -0300

    feat: add ills

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CSSNormalize from "ui/CSSNormalize";
 import Header from "app/Header";
 import Intro from "app/Intro";
 import { CSSThemeVars, theme } from "utils/theme";
+import { ReactComponent as People } from "assets/People.svg";
 
 export default function App({
   children = (
@@ -13,6 +14,7 @@ export default function App({
       <CSSGlobalStyles />
       <Header />
       <Intro />
+      <People />
     </>
   ),
 }: React.PropsWithChildren<Record<string, unknown>>) {
