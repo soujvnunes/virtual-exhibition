4dca7d5935fa1dc659be56c7a0a13acd0fa0a3d3
soujvnunes <soujvnunes@gmail.com>
2023-07-13 18:06:46 -0300

    feat: adjust utils

--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export type PropsWithAs<
-  P extends Record<string, unknown>,
-  E extends keyof JSX.IntrinsicElements | React.ComponentType<P> =
-    | keyof JSX.IntrinsicElements
-    | React.ComponentType<P>,
-> = {
-  as?: E;
-  forwardedAs?: E;
-} & P;
