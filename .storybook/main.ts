1abb3173d9f33fa569ebc793d17dcd172423f5ec
soujvnunes <soujvnunes@gmail.com>
2023-07-13 18:55:43 -0300

    feat: integrate tailwind with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,12 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
