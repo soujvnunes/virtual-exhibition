7286566f1287d1229d86927e6f6c85ab73f1d530
soujvnunes <soujvnunes@gmail.com>
2023-07-16 12:36:24 -0300

    feat: set neta theme color dinamically

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import CSSGlobalStyles from "ui/CSSGlobalStyles";
 import CSSNormalize from "ui/CSSNormalize";
 import Header from "app/Header";
 import Intro from "app/Intro";
+import createMeta from "utils/createMeta";
 import { CSSThemeVars, theme } from "utils/theme";
 import { ReactComponent as People } from "assets/People.svg";
 
 export default function App({
   children = (
     <>
-      <CSSThemeVars />
-      <CSSNormalize />
-      <CSSGlobalStyles />
       <Header />
       <Intro />
       <People />
     </>
   ),
 }: React.PropsWithChildren<Record<string, unknown>>) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  createMeta({
+    name: "theme-color",
+    content: `rgb(${theme.channel.background})`,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSThemeVars />
+      <CSSNormalize />
+      <CSSGlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
 }
