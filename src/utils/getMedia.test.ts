38995e6751c3d9de9aca6ca1a240db88a64129a3
soujvnunes <soujvnunes@gmail.com>
2023-07-16 12:35:15 -0300

    feat: simplify useMedia

--- src/utils/getMedia.test.ts
+++ src/utils/getMedia.test.ts
@@ -1,8 +1,6 @@
-import { renderHook } from "@testing-library/react-hooks";
+import getMedia from "./getMedia";
 
-import useMedia from "./useMedia";
-
-function mockMatchMedia({ matches }: { matches: boolean }) {
+function mockMatchMedia({ matches }: Record<"matches", boolean>) {
   Object.defineProperty(window, "matchMedia", {
     writable: true,
     value: jest.fn().mockImplementation((query) => ({
@@ -15,33 +13,23 @@ function mockMatchMedia({ matches }: { matches: boolean }) {
     })),
   });
 }
-function renderMedia() {
-  return renderHook(({ query }) => useMedia(query), {
-    initialProps: {
-      query: "(min-width: 768px)",
-    },
-  });
-}
+
+const matchesMedia = getMedia("min-width: 780px");
+
 it("returns true if media query matches", () => {
   mockMatchMedia({ matches: true });
 
-  const { result } = renderMedia();
-
-  expect(result.current).toBe(true);
+  expect(matchesMedia).toBe(true);
 });
 it("returns false if media query doesn't matches", () => {
   mockMatchMedia({ matches: false });
 
-  const { result } = renderMedia();
-
-  expect(result.current).toBe(false);
+  expect(matchesMedia).toBe(false);
 });
 it("returns either true of false if media query changes", () => {
   mockMatchMedia({ matches: false });
 
-  const { result } = renderMedia();
-
-  expect(result.current).toBe(false);
+  expect(matchesMedia).toBe(false);
   mockMatchMedia({ matches: true });
-  expect(result.current).toBe(true);
+  expect(matchesMedia).toBe(true);
 });
